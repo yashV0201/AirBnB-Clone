2c2ef795783a72d82d87c4811d26f2106e65b8ab
yashV0201 <vardhan.yash0201@gmail.com>
2024-04-11 19:24:21 +0530

    Built basic backend setup, connected to DB, performed CRUD operations on Listings using RESTful APIs

---
+++ app.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
+
+main().then(()=>{
+    console.log("Connected to DB");
+}).catch(err=>{
+    console.log(err);
+})
+
+async function main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+}
+
+app.get("/",(req,res)=>{
+    res.send("root working fine")
+});
+
+//INDEX ROUTE
+app.get("/listings",async (req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
+})
+
+//NEW ROUTE
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+});
+
+//CREATE ROUTE
+app.post("/listings",async (req,res)=>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+})
+
+
+//SHOW ROUTE
+app.get("/listings/:id",async (req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+})
+
+//EDIT ROUTE
+app.get("/listings/:id/edit",async (req,res)=>{
+    let {id} = req.params;
+    let listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+})
+
+//UPDATE ROUTE
+app.put("/listings/:id",async (req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect("/listings");
+})
+
+//DELETE ROUTE
+app.delete("/listings/:id", async (req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+})
+
+
+
+
+
+
+
+// app.get("/testListing",async (req,res)=>{
+//     let sampleListing = new Listing({
+//         title:"My New Villa",
+//         description:"BY the beach",
+//         price: 1200,
+//         location:"Goa",
+//         country:"India"
+//     })
+
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("testting successful");
+// })
+
+
+
+app.listen(8080,()=>{
+    console.log("server is listening to port 8080");
+})
