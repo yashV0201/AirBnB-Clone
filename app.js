2a74e0901117aabdba6dd06202f32e95ffa5fd96
yashV0201 <vardhan.yash0201@gmail.com>
2024-04-12 02:37:08 +0530

    styled the show page using bootstrap cards and tewaked the styling of buttons and alignments of texts

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.get("/listings/:id/edit",async (req,res)=>{
 app.put("/listings/:id",async (req,res)=>{
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect("/listings");
+    res.redirect(`/listings/${id}`);
 })
 
 //DELETE ROUTE
