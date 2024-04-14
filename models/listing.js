88b0b54d3a21716b6a8df89fabc3946f2e970f5b
yashV0201 <vardhan.yash0201@gmail.com>
2024-04-14 20:39:47 +0530

    added a mongoose middleware to delete all the reviews of a listing from the database when the listing itself is deleted, we used post operation middleware on listingSchema which listens to findOneAndDelete and performs operations on Review Model

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,7 @@
 const  mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
+
 
 const listingSchema = new Schema({
     title:{
@@ -22,6 +24,13 @@ const listingSchema = new Schema({
         }
     ]
 })
+
+listingSchema.post("findOneAndDelete", async(listing)=>{
+    if(listing){
+        await Review.deleteMany({_id:{$in: listing.reviews}});
+    }
+    
+});
 
 const Listing = mongoose.model("listing",listingSchema);
 
