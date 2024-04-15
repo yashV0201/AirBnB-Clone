d77b2e148cb3ced42c0a2653fa1c828ccfd748ab
yashV0201 <vardhan.yash0201@gmail.com>
2024-04-15 23:56:55 +0530

    initialised passport, local strategy, built a new user model and added passport local mongoose as a plugin to the userSchema which automatically adds a username and password to the user without actually defining them in the schema

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const listings= require("./routes/listings.js")
 const reviews = require("./routes/reviews.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require('passport');
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js")
 
 
 app.set("view engine","ejs");
@@ -51,10 +54,29 @@ app.get("/",(req,res)=>{
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
 app.use((req,res,next)=>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
+})
+
+app.get("/demouser", async (req,res)=>{
+    let fakeUser = new User({
+        email:"kkk@gmail.com",
+        username:"delta-stu"
+    })
+
+    let registeredUser = await User.register(fakeUser,"ossumm");
+    res.send(registeredUser);
 })
 
 //LISTINGS
