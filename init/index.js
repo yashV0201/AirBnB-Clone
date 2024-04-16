4893bb7d3b1213a32f8218e2d67cbc102d88d563
yashV0201 <vardhan.yash0201@gmail.com>
2024-04-17 04:22:48 +0530

    reinitiaized the lisings and included a owner in them

--- init/index.js
+++ init/index.js
@@ -14,6 +14,7 @@ async function main(){
 
 const initDB = async ()=>{
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj)=>({...obj,owner:'661d98a3e12fced2c6a0f873'}));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 }
